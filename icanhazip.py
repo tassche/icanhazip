2e6550af46273eae6f0373d271aace843bc1375c
Tijl Van Assche <tijlvanassche@gmail.com>
2014-06-15 16:39:28 +0200

    Initial commit
    icanhazip.py
      * Gets the external IP address via icanhazip.com.
      * Compares the IP with the IP in icanhazip.ini.
      * If the IP has changed, save the IP in icanhazip.ini and send an
        e-mail message.
    
    setup.py
      * Creates a sample icanhazip.ini config file.

---
+++ icanhazip.py
@@ -0,0 +1,53 @@
+from email.mime.text import MIMEText
+from smtplib import SMTP
+import configparser
+import urllib.request
+
+config_file = 'icanhazip.ini'
+
+def get_ip_address():
+    request = urllib.request.Request('http://icanhazip.com')
+    response = urllib.request.urlopen(request)
+    ip_addr = str(response.read(), 'utf-8').replace('\n', '')
+    response.close()
+    return ip_addr
+
+def send_email(from_addr, to_addr, subject, body, server, port):
+    message = MIMEText(body)
+    message['From'] = from_addr
+    message['To'] = to_addr
+    message['Subject'] = subject
+    with SMTP(server, port) as smtp:
+        smtp.send_message(message)
+
+def read_config():
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    return config
+
+def write_config(config):
+    with open(config_file, 'w') as ini:
+        config.write(ini)
+
+if __name__ == '__main__':
+    config = read_config()
+
+    try:
+        current_ip_addr = config['icanhazip']['ip_addr']
+    except KeyError:
+        config['icanhazip'] = {'ip_addr': ''}
+        write_config(config)
+        current_ip_addr = None
+
+    ip_addr = get_ip_address()
+
+    if ip_addr != current_ip_addr:
+        email = config['e-mail']
+        subject = email['subject'].format(ip=ip_addr)
+        body = email['body'].format(ip=ip_addr)
+        send_email(email['from'], email['to'], subject, body, 
+                   email['smtp_server'], email['smtp_port'])
+
+        config['icanhazip']['ip_addr'] = ip_addr
+        write_config(config)
+
